02b7e54789557de71c62ce97ccaac3bb057f2bb5
Trevor <tlist403@gmail.com>
2021-11-24 20:15:50 -0300

    Completed tests

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,5 @@
+#require './spec/tic_tac_toe_spec.rb'
+
 class Game
   attr_accessor :name, :token, :board, :turn_counter
 
@@ -7,14 +9,18 @@ class Game
   end
 
   def play(current_player)
-    @turn_counter = 0
-    @board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    setup_game
     puts "Type 'Q' at any time to quit."
     player_input(current_player)
   end
 
+  def setup_game
+    @turn_counter = 0
+    @board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+  end
+
   def player_input(player1, current_player=player1)
-    player2 = Game.new('Player2', '0')
+    player2 = Game.new('Player2', 'O')
     loop do
       if @turn_counter.even?
         current_player = player1
@@ -26,8 +32,11 @@ class Game
       print_grid
       input = gets.chomp.downcase
       change_board(current_player, input)
-      game_over?(current_player)
-      tied_game?
+      win(current_player) if game_over?(current_player) == true
+      break if game_over?(current_player) == true
+
+      tie if tied_game? == true
+      break if tied_game? == true
       break if input == 'q'
     end
   end
@@ -55,33 +64,29 @@ class Game
 
   def game_over?(current_player)
     # 1, 2, 3
-    win(current_player) if board[0].all? { |x| x == current_player.token }
+    return true if board[0].all? { |x| x == current_player.token }
     # 4, 5, 6
-    win(current_player) if board[1].all? { |x| x == current_player.token }
+    return true if board[1].all? { |x| x == current_player.token }
     # 7, 8, 9
-    win(current_player) if board[2].all? { |x| x == current_player.token }
+    return true if board[2].all? { |x| x == current_player.token }
     # 1, 5, 9
-    if board[0][0] == board[1][1] && board[1][1] == board[2][2]
-      win(current_player)
-    end
+    return true if board[0][0] == board[1][1] && board[1][1] == board[2][2]
     # 3, 5, 7
-    if board[0][2] == board[1][1] && board[1][1] == board[2][0]
-      win(current_player)
-    end
+    return true if board[0][2] == board[1][1] && board[1][1] == board[2][0]
+
     # 1, 4, 7
     # 2, 5, 8
     # 3, 6, 9
     (0...3).each do |spot|
-      if board[0][spot] == board[1][spot] && board[1][spot] == board[2][spot]
-        win(current_player)
-      end
+      return true if board[0][spot] == board[1][spot] && board[1][spot] == board[2][spot]
     end
+    
+    return false
   end
 
   def win(current_player)
     print_grid
     puts "Hurray! #{current_player.name} won!"
-    exit
   end
 
   def tied_game?
@@ -94,11 +99,11 @@ class Game
         end
       end
     end
+    all_spots_filled
+  end
 
-    if all_spots_filled == true
-      puts 'You tied :/'
-      exit
-    end
+  def tie
+    puts 'You tied :/'
   end
 end
 
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -2,9 +2,94 @@
 require './lib/tic_tac_toe.rb'
 
 describe Game do 
+  subject(:game) { described_class.new('Player1', 'X') }
+  let(:player2) { described_class.new('Player2', 'O') }
+  let(:board) { game.instance_variable_get(:@board) }
+
   describe '#change_board' do
-    context 'takes in input' do
-      it 'changes the value of a spot matching input' do
+    before do
+      game.setup_game
+    end
+
+    context 'when selecting a free spot' do
+      it 'changes a spot to the player token' do        expect { game.change_board(game, '3') }.to change { game.board[0][2] }.to('X')
+      end
+    end
+
+    context 'when selecting an already selected spot' do 
+      before do 
+        game.change_board(game, '3')
+      end
+
+      it 'does not change the spot' do 
+        expect { game.change_board(player2, '3') }.to_not change { game.board[0][2] }
+      end
+    end
+
+    context 'when improper input is given' do 
+      it 'does not change the spot' do 
+        expect { game.change_board(game, 'd') }.to_not change { game.board }
+      end
+    end
+  end
+
+  describe '#game_over?' do 
+    context 'when top row is X X X' do 
+      it 'returns true' do 
+        game.board = [['X', 'X', 'X'], [4, 5, 6], [7, 8, 9]]
+        expect(game.game_over?(game)).to be true
+      end
+    end
+
+    context 'when middle row is O O O' do
+      it 'returns true' do 
+        game.board = [[1, 2, 3], ['O', 'O', 'O'], [7, 8, 9]]
+        expect(game.game_over?(player2)).to be true
+      end
+    end
+
+    context 'when bottom row is X X X' do 
+      it 'returns true' do
+        game.board = [[1, 2, 3], [4, 5, 6], ['X', 'X', 'X']]
+        expect(game.game_over?(game)).to be true
+      end
+    end
+
+    context 'when diagonal is O O O' do 
+      it 'returns true' do 
+        game.board = [['O', 2, 3], [4, 'O', 6], [7, 8, 'O']]
+        expect(game.game_over?(player2)).to be true
+      end
+    end
+
+    context 'when vertical line is X X X' do 
+      it 'returns true' do 
+        game.board = [[1, 2, 'X'], [4, 5, 'X'], [7, 8, 'X']]
+        expect(game.game_over?(game)).to be true
+      end
+    end
+
+    context 'when there is not three in a row' do 
+      it 'returns false' do
+        game.board = [['O', 2, 3], ['X', 5, 'O'], ['X', 'X', 9]]
+        expect(game.game_over?(game)).to be false
+      end
+    end
+  end
+
+  describe '#tied_game?' do 
+    context 'all spots are filled and no winner' do 
+      it 'returns true' do 
+        game.board = [%w(O O X), %w(X X O), %w(X X O)]
+        expect(game.tied_game?).to be true
+      end
+    end
+
+    context 'not all spots are filled' do 
+      it 'returns false' do 
+        game.board = [['O', 2, 3], ['X', 5, 'O'], ['X', 'X', 9]]
+        expect(game.tied_game?).to be false
+      end
     end
   end
 end
