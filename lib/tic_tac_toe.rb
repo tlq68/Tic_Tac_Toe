e965421ec8ff0e170287e2b70b66d01740efde2e
Trevor <tlist403@gmail.com>
2021-11-24 13:41:51 -0300

    Code refactor for easier tests

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,173 +1,107 @@
 class Game
-    attr_accessor :spot1, :spot2, :spot3, :spot4, :spot5, :spot6, :spot7, :spot8, :spot9
-    attr_accessor :input
+  attr_accessor :name, :token, :board, :turn_counter
 
-    attr_accessor :name, :token
+  def initialize(name, token)
+    @name = name
+    @token = token
+  end
 
-    @@turn_counter = 0
-    @@winner = false
+  def play(current_player)
+    @turn_counter = 0
+    @board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    puts "Type 'Q' at any time to quit."
+    player_input(current_player)
+  end
 
-    
+  def player_input(player1, current_player=player1)
+    player2 = Game.new('Player2', '0')
+    loop do
+      if @turn_counter.even?
+        current_player = player1
+      elsif @turn_counter.odd?
+        current_player = player2
+      end
 
-    def initialize(name, token) 
-        @name = name
-        @token = token
+      puts "Make a choice, #{current_player.name}."
+      print_grid
+      input = gets.chomp.downcase
+      change_board(current_player, input)
+      game_over?(current_player)
+      tied_game?
+      break if input == 'q'
     end
+  end
 
-    def input
-        @input = input
-    end
-    
-    def spots
-        @spot1 = "1"
-        @spot2 = "2"
-        @spot3 = "3"
-        @spot4 = "4"
-        @spot5 = "5"
-        @spot6 = "6"
-        @spot7 = "7"
-        @spot8 = "8"
-        @spot9 = "9" 
-    end
+  def print_grid
+    puts ''
+    puts "  #{@board[0][0]} | #{@board[0][1]} | #{@board[0][2]} "
+    puts ' ---|---|--- '
+    puts "  #{@board[1][0]} | #{@board[1][1]} | #{@board[1][2]} "
+    puts ' ---|---|--- '
+    puts "  #{@board[2][0]} | #{@board[2][1]} | #{@board[2][2]} "
+    puts ''
+  end
 
-    def player_input(player1, player2, current_player=player1)
-
-        loop do 
-
-            if @@turn_counter % 2 == 0
-                current_player = player1
-            elsif @@turn_counter % 2 != 0
-                current_player = player2
-            end
-
-            puts "Make a choice, #{current_player.name}."
-            input = gets.chomp.downcase
-            choices =   
-                case input
-                when '1'
-                     if @spot1 == '1'
-                        @spot1 = current_player.token
-                        @@turn_counter += 1
-                     end
-                when '2'
-                    if @spot2 == '2'
-                        @spot2 = current_player.token
-                        @@turn_counter += 1
-                     end
-                when '3'
-                    if @spot3 == '3'
-                        @spot3 = current_player.token
-                        @@turn_counter += 1
-                     end
-                when '4'
-                    if @spot4 == '4'
-                        @spot4 = current_player.token
-                        @@turn_counter += 1
-                    end
-                when '5'
-                    if @spot5 == '5'
-                        @spot5 = current_player.token
-                        @@turn_counter += 1
-                    end
-                when '6'
-                    if @spot6 == '6'
-                        @spot6 = current_player.token
-                        @@turn_counter += 1
-                    end
-                when '7'
-                    if @spot7 == '7'
-                        @spot7 = current_player.token
-                        @@turn_counter += 1
-                    end
-                when '8'
-                    if @spot8 == '8'
-                        @spot8 = current_player.token
-                        @@turn_counter += 1
-                    end
-                when '9'
-                    if @spot9 == '9'
-                        @spot9 = current_player.token
-                        @@turn_counter += 1
-                    end
-                else 
-                    puts "\nNot a good input"
-                end    
-                
-                print_grid
-
-                if @spot1 == current_player.token && @spot2 == current_player.token && @spot3 == current_player.token
-                    puts "#{current_player.name} won!"
-                    break
-                end
-
-                if @spot4 == current_player.token && @spot5 == current_player.token && @spot6 == current_player.token
-                    puts "#{current_player.name} won!"
-                    break
-                end
-
-                if @spot7 == current_player.token && @spot8 == current_player.token && @spot9 == current_player.token
-                    puts "#{current_player.name} won!"
-                    break
-                end
-
-                if @spot1 == current_player.token && @spot4 == current_player.token && @spot7 == current_player.token
-                    puts "#{current_player.name} won!"
-                    break
-                end
-
-                if @spot2 == current_player.token && @spot5 == current_player.token && @spot8 == current_player.token
-                    puts "#{current_player.name} won!"
-                    break
-                end
-
-                if @spot3 == current_player.token && @spot6 == current_player.token && @spot9 == current_player.token
-                    puts "#{current_player.name} won!"
-                    break
-                end
-
-                if @spot1 == current_player.token && @spot5 == current_player.token && @spot9 == current_player.token
-                    puts "#{current_player.name} won!"
-                    break
-                end
-
-                if @spot3 == current_player.token && @spot5 == current_player.token && @spot7 == current_player.token
-                    puts "#{current_player.name} won!"
-                    break
-                end
-
-                if @spot1 != '1' && @spot2 != '2' && @spot3 != '3' && @spot4 != '4' && @spot5 != '5' && @spot6 != '6' && @spot7 != '7' && @spot8 != '8' && @spot9 != '9'
-                    puts "You tied :/"
-                    break
-                end
-            break if input == 'q'
+  def change_board(current_player, input)
+    (0...3).each do |row|
+      (0...3).each do |spot|
+        if input.to_i == board[row][spot]
+          board[row][spot] = current_player.token
+          @turn_counter += 1
         end
+      end
+    end
+  end
+
+  def game_over?(current_player)
+    # 1, 2, 3
+    win(current_player) if board[0].all? { |x| x == current_player.token }
+    # 4, 5, 6
+    win(current_player) if board[1].all? { |x| x == current_player.token }
+    # 7, 8, 9
+    win(current_player) if board[2].all? { |x| x == current_player.token }
+    # 1, 5, 9
+    if board[0][0] == board[1][1] && board[1][1] == board[2][2]
+      win(current_player)
+    end
+    # 3, 5, 7
+    if board[0][2] == board[1][1] && board[1][1] == board[2][0]
+      win(current_player)
+    end
+    # 1, 4, 7
+    # 2, 5, 8
+    # 3, 6, 9
+    (0...3).each do |spot|
+      if board[0][spot] == board[1][spot] && board[1][spot] == board[2][spot]
+        win(current_player)
+      end
+    end
+  end
+
+  def win(current_player)
+    print_grid
+    puts "Hurray! #{current_player.name} won!"
+    exit
+  end
+
+  def tied_game?
+    all_spots_filled = true
+    board.each do |row|
+      row.each do |spot|
+        if spot.is_a?(Integer)
+          all_spots_filled = false
+          break
+        end
+      end
     end
 
-    def print_grid
-        puts ""
-        puts "  #{@spot1} | #{@spot2} | #{@spot3} "
-        puts " ---|---|--- "
-        puts "  #{@spot4} | #{@spot5} | #{@spot6} "
-        puts " ---|---|--- "
-        puts "  #{@spot7} | #{@spot8} | #{@spot9} "
-        puts ""
+    if all_spots_filled == true
+      puts 'You tied :/'
+      exit
     end
-
-    def play(current_player, next_player)
-        spots
-        print_grid
-        puts "Type 'Q' at any time to quit."
-        player_input(current_player, next_player)
-    end
-
-    def win(current_player)
-       puts "Hurray! #{@current_player.name} won!" 
-    end
+  end
 end
 
+player1 = Game.new('Player1', 'X')
 
-
-player1 = Game.new("Player1", "X")
-player2 = Game.new("Player2", "O")
-
-player2.play(player1, player2)
+player1.play(player1)
---
+++ spec/tic_tac_toe_spec.rb
@@ -0,0 +1,10 @@
+# spec/tic_tac_toe_spec.rb
+require './lib/tic_tac_toe.rb'
+
+describe Game do 
+  describe '#change_board' do
+    context 'takes in input' do
+      it 'changes the value of a spot matching input' do
+    end
+  end
+end
